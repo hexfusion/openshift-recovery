344df03406b454738c834adee73bc2c173598157
Sam Batschelet <sbatsche@redhat.com>
2019-05-15 09:15:28 -0400

    typo
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- bin/etcd-member-recover.sh
+++ bin/etcd-member-recover.sh
@@ -39,8 +39,8 @@ ETCD_VERSION=v3.3.10
 ETCD_DATA_DIR=/var/lib/etcd
 ETCD_STATIC_RESOURCES="${CONFIG_FILE_DIR}/static-pod-resources/etcd-member"
 
-SHARED=/usr/local/shared/openshift-recovery
-TEMPLATE="$SHARED/templates/etcd-generate-certs.yaml.template"
+SHARED=/usr/local/share/openshift-recovery
+TEMPLATE="$SHARED/template/etcd-generate-certs.yaml.template"
 
 source "/usr/local/bin/openshift-recovery-tools"
 
